3d57dacb296a565bc12281b2719e67a0e16eb969
Giorgio Natili <g.natili@gnstudio.com>
2025-08-12 17:35:14 -0700

    tests(config): exclude Playwright E2E from Jest via testPathIgnorePatterns

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
+  testPathIgnorePatterns: ['<rootDir>/src/e2e/'],
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
     '**/*.(test|spec).+(ts|tsx|js)'
